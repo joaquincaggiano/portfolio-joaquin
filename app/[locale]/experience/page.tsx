7f0bd099081ee9e55f2f4a3e38abd2c3a55af250
joaquincaggiano <joaquincaggiano@gmail.com>
2024-08-07 18:41:33 +0200

    card experience

--- app/[locale]/experience/page.tsx
+++ app/[locale]/experience/page.tsx
@@ -1,13 +1,17 @@
-import {useTranslations} from 'next-intl';
+import CardExperiencie from "@/components/cards/card-experiencie";
+import { useTranslations } from "next-intl";
 
 const ExperiencePage = () => {
   const t = useTranslations();
   return (
     <div>
-      {/* <h1 className='text-xl'>{t("Home.title")}</h1> */}
-      Experience
+      <div className="flex flex-col gap-10 sm:flex-row justify-center items-center">
+        <CardExperiencie data="5" title={t("Experience.coursesTaken")} />
+        <CardExperiencie data="+12" title={t("Experience.projectsDone")} />
+        <CardExperiencie data="18" title={t("Experience.monthsOfExperience")} />
+      </div>
     </div>
   );
-}
+};
 
 export default ExperiencePage;
---
+++ components/cards/card-experiencie.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Card, CardBody } from "@nextui-org/react";
+
+interface Props {
+    title: string;
+  data: string;
+}
+
+const CardExperiencie = ({ data, title }: Props) => {
+  return (
+    <Card className="w-[250px] border-gray-700 border-1">
+      <CardBody>
+        <div className="flex flex-col justify-center items-center gap-2.5">
+          <div className="text-6xl">{data}</div>
+          <span className="text-xs">{title}</span>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default CardExperiencie;
